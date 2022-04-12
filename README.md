f33ee0b9352ca10378578d0e781d790f975e21a7
swain <iamswain@hotmail.com>
2022-04-12 13:05:54 +0900

    행정동 -> 시군으로 변경

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ https://github.com/vuski/admdongkor/blob/master/ver20220309/HangJeongDong_ver202
 
 npm i -g maketypes
 make_types -i data/interfaces.d.ts data/HangJeongDong_ver20220309.geojson RootData
+
+## 시군구 데이터
+
+https://neurowhai.tistory.com/350
+
+## map merging
+
+https://mjs1995.tistory.com/169
--- data/interfaces.d.ts
+++ data/interfaces.d.ts
@@ -2,8 +2,8 @@ export interface RootData {
   type: string;
   name: string;
   crs: Crs;
-  bbox?: (number)[] | null;
-  features?: (FeaturesEntity)[] | null;
+  bbox?: number[] | null;
+  features?: FeaturesEntity[] | null;
 }
 export interface Crs {
   type: string;
@@ -15,18 +15,22 @@ export interface Properties {
 export interface FeaturesEntity {
   type: string;
   properties: Properties1;
-  bbox?: (number)[] | null;
+  bbox?: number[] | null;
   geometry: Geometry;
 }
 export interface Properties1 {
   OBJECTID: number;
+  /** 속성의 adm_nm 은 통계청에서 사용하는 전국 행정동 이름입니다. */
   adm_nm: string;
+  /** 속성의 adm_cd 는 통계청에서 사용하는 7자리의 [한국행정구역분류코드]입니다. */
   adm_cd: string;
+  /** 속성의 adm_cd2 는 행정안전부 사용하는 10자리의 [행정기관코드]입니다. */
   adm_cd2: string;
   sgg: string;
   sido: string;
   sidonm: string;
   sggnm: string;
+  /** 속성의 adm_cd8 는 통계청에서 사용하는 8자리의 새로운 [한국행정구역분류코드]입니다. */
   adm_cd8: string;
   emd_nm: string;
   ELEC_CD: string;
@@ -36,5 +40,5 @@ export interface Properties1 {
 }
 export interface Geometry {
   type: string;
-  coordinates?: ((((number)[] | null)[] | null)[] | null)[] | null;
+  coordinates?: (((number[] | null)[] | null)[] | null)[] | null;
 }
